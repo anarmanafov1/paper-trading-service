73cddd4891e2fc6714872798ce8dc36aaa74edff
Anar Manafov <anarmanafov1@gmail.com>
2023-07-03 16:59:03 -0400

    works

--- src/main/scala/com/papertrader/service/models/GlobalQuote.scala
+++ src/main/scala/com/papertrader/service/models/GlobalQuote.scala
@@ -1,3 +1,24 @@
 package com.papertrader.service.models
 
-case class GlobalQuote(name: String)
+import io.circe.{Decoder, HCursor}
+
+case class GlobalQuote(
+  symbol: String,
+  price: BigDecimal,
+  low: BigDecimal,
+  high: BigDecimal
+)
+
+object Decoders {
+  implicit val decodeGlobalQuote: Decoder[GlobalQuote] = new Decoder[GlobalQuote] {
+    final def apply(c: HCursor): Decoder.Result[GlobalQuote] = {
+      val innerObject = c.downField("Global Quote")
+      for {
+        symbol <- innerObject.downField("01. symbol").as[String]
+        price <- innerObject.downField("05. price").as[BigDecimal]
+        low <- innerObject.downField("04. low").as[BigDecimal]
+        high <- innerObject.downField("03. high").as[BigDecimal]
+      } yield GlobalQuote(symbol, price, low, high)
+    }
+  }
+}
