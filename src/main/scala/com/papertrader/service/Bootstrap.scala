1ad8838d545a2eea029aaab05c9fcd2f8447279c
Anar Manafov <anarmanafov1@gmail.com>
2023-07-04 13:40:21 -0400

    wip

--- src/main/scala/com/papertrader/service/Bootstrap.scala
+++ src/main/scala/com/papertrader/service/Bootstrap.scala
@@ -8,30 +8,29 @@ import org.http4s.client.{Client, JavaNetClientBuilder}
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import com.papertrader.service.conf.logger.ApplicationLogger
+import org.typelevel.log4cats.Logger
+
 object Bootstrap {
   def all: IO[Unit] = {
     for {
-      implicit0(appConf: ApplicationConfig) <- Resource.eval(
-        IO.fromEither(
-          ApplicationConfig
-            .load
-            .left
-            .map(e => new Throwable(e.prettyPrint()))
-        )
-      )
       implicit0(l: org.typelevel.log4cats.Logger[IO]) <- Resource.pure(ApplicationLogger.getLogger[IO])
+      implicit0(appConf: ApplicationConfig) <- loadApplicationConfig()
       implicit0(c: Client[IO]) <- Resource.pure(JavaNetClientBuilder[IO].create)
-
-      httpApp = (
-        PapertraderRoutes.routes()
-        ).orNotFound
-
       _ <-
         EmberServerBuilder.default[IO]
           .withHost(ipv4"0.0.0.0")
           .withPort(port"8080")
-          .withHttpApp(httpApp)
+          .withHttpApp(PapertraderRoutes.routes().orNotFound)
           .build
     } yield ()
   }.useForever
+
+  private def loadApplicationConfig()(implicit logger: Logger[IO]): Resource[IO, ApplicationConfig] =
+    Resource.eval(
+      IO.fromEither(
+        ApplicationConfig
+          .load
+          .left
+          .map(e => new Throwable(e.prettyPrint()))
+      ).handleErrorWith(e => logger.error(e.getMessage) >> IO.raiseError(e)))
 }
