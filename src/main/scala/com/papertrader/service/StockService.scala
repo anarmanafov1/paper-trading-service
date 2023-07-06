fd44d062d126a24778a3bf93a3f5920f6d7e993c
Anar Manafov <anarmanafov1@gmail.com>
2023-07-06 16:08:43 -0400

    fixed errors

--- src/main/scala/com/papertrader/service/StockService.scala
+++ src/main/scala/com/papertrader/service/StockService.scala
@@ -13,17 +13,15 @@ class StockService[F[_]](alphaVantageStockClient: AlphaVantageStockClient[F], ba
     alphaVantageStockClient.getGlobalQuote(symbol)
   }
 
-  // TODO: Review and simplify map logic
-  def addToBasket(symbol: String, quantity: Int, userId: UUID): F[Unit] = ???
-//  {
-//    basketRef.update(userToBasket => userToBasket.get(userId) match {
-//      case Some(basket) if basket.contains(symbol) =>
-//        val currentQuantity = basket(symbol)
-//        userToBasket ++ Map[UUID, Map[String, Int]](userId, basket ++ Map(symbol -> currentQuantity))
-//      case Some(basket) => userToBasket ++ Map[UUID, Map[String, Int]](userId -> (basket ++ Map[String, Int](symbol, quantity)))
-//      case None => userToBasket ++ Map[UUID, Map[String, Int]](userId, Map.empty)
-//    })
-//  }
+  def addToBasket(symbol: String, quantity: Int, userId: UUID): F[Unit] = {
+    basketRef.update(
+      userToBasket => userToBasket.get(userId) match {
+        case Some(basket) if basket.contains(symbol) => userToBasket ++ Map(userId -> (basket ++ Map(symbol -> basket(symbol))))
+        case Some(basket) => userToBasket ++ Map(userId -> (basket ++ Map(symbol -> quantity)))
+        case None => userToBasket ++ Map(userId -> Map.empty)
+      }
+    )
+  }
 
   def viewBasket(userId: UUID)(implicit me: MonadError[F, Throwable]): F[Map[String, Int]] = basketRef.get.map(basket => basket.getOrElse(userId, Map.empty))
 }
