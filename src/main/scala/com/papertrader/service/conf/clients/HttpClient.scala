7fcd0608199d2ba8bc141559f3c2888e65e2b42c
Anar Manafov <anarmanafov1@gmail.com>
2023-07-06 12:46:43 -0400

    clean up

--- src/main/scala/com/papertrader/service/conf/clients/HttpClient.scala
+++ src/main/scala/com/papertrader/service/conf/clients/HttpClient.scala
@@ -1,6 +1,7 @@
 package com.papertrader.service.conf.clients
 
 import cats.MonadError
+import cats.implicits._
 import com.papertrader.service._
 import org.http4s.{EntityDecoder, Status, Uri}
 import org.http4s.client.Client
@@ -12,9 +13,17 @@ trait HttpClient {
   def get[F[_], A](uri: Uri)(implicit client: Client[F], entityDecoder: EntityDecoder[F, A], logger: Logger[F], me: MonadError[F, Throwable]): F[A] = {
     val uriForLog = uri.withQueryParam("apikey", "####").renderString
     client.get[A](uri) {
-      case Status.Successful(r) => me.flatMap(logger.error(s"Got OK response for request: $uriForLog"))(_ => me.flatMap(r.as[A])(_ => me.raiseError(StockClientParseError)))
-      case r if r.status == Status.NotFound => me.flatMap(logger.error(s"Got NotFound response for request with api key removed: $uriForLog, responding with $StockClientNotFoundError"))(_ => me.raiseError(StockClientNotFoundError))
-      case v => me.flatMap(logger.error(s"Unhandled response with status ${v.status}, responding with $StockClientServerError"))(_ => me.raiseError(StockClientServerError))
+      case Status.Successful(r) =>
+        for {
+          _ <- logger.error(s"Got OK response for request: $uriForLog")
+          parsed <- r.as[A].handleErrorWith(_ => me.raiseError(StockClientParseError))
+        } yield parsed
+      case r if r.status == Status.NotFound =>
+        logger.error(s"Got NotFound response for request with api key removed: $uriForLog, responding with $StockClientNotFoundError")
+          .flatMap(_ => me.raiseError(StockClientNotFoundError))
+      case v =>
+        logger.error(s"Unhandled response with status ${v.status}, responding with $StockClientServerError")
+          .flatMap(_ => me.raiseError(StockClientServerError))
     }
   }
 }
--- src/main/scala/com/papertrader/service/conf/routes/PapertraderRoutes.scala
+++ src/main/scala/com/papertrader/service/conf/routes/PapertraderRoutes.scala
@@ -1,6 +1,7 @@
 package com.papertrader.service.conf.routes
 
 import cats.MonadError
+import cats.implicits.catsSyntaxFlatMapOps
 import com.papertrader.service._
 import io.circe.generic.auto.exportEncoder
 import io.circe.syntax.EncoderOps
@@ -10,18 +11,19 @@ import org.http4s.circe._
 
 object PapertraderRoutes {
 
-  def routes[F[_]](stockService: StockService[F])(implicit me: MonadError[F, Throwable]): HttpRoutes[F] = {
+  def routes[F[_]](stockService: StockService[F])(implicit me: MonadError[F, Throwable], logger: org.typelevel.log4cats.Logger[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / "global-quote" / symbol =>
         me.flatMap(me.attempt(stockService.getGlobalQuote(symbol))) {
-          case Left(StockClientNotFoundError) => NotFound(s"Stock with symbol $symbol not found.")
-          case Left(StockClientServerError | StockClientParseError) => InternalServerError("Something went wrong.")
           case Right(v) => Ok(v.asJson)
-          case _ =>
-            println("suppress errors") //TODO: Fix
-            NoContent()
+          case Left(StockClientNotFoundError) => NotFound(s"Stock with symbol $symbol not found.")
+          case Left(StockClientServerError) => ServiceUnavailable("Failed to retrieve stock.")
+          case Left(StockClientParseError) => InternalServerError("Error processing stock.")
+          case Left(e: Throwable) =>
+            e.printStackTrace()
+            logger.error(e.getMessage) >> InternalServerError("Something went wrong.")
         }
     }
   }
