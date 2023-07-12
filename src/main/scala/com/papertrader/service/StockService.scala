1be3395270b538a9f4a851ae0f4462b492de56f4
Anar Manafov <anarmanafov1@gmail.com>
2023-07-11 21:29:10 -0400

    fixed basket insertion logic

--- src/main/scala/com/papertrader/service/StockService.scala
+++ src/main/scala/com/papertrader/service/StockService.scala
@@ -5,23 +5,26 @@ import cats.effect.Ref
 import cats.implicits._
 import com.papertrader.service.models.{GlobalQuote, Item}
 import com.papertrader.service.util.clients.AlphaVantageStockClient
-
 import java.util.UUID
 
 class StockService[F[_]](alphaVantageStockClient: AlphaVantageStockClient[F], basketRef: Ref[F, Map[UUID, Map[String, Int]]]) {
-  def getGlobalQuote(symbol: String): F[GlobalQuote] = {
-    alphaVantageStockClient.getGlobalQuote(symbol)
-  }
+  def getGlobalQuote(symbol: String): F[GlobalQuote] = alphaVantageStockClient.getGlobalQuote(symbol)
 
-  def addToBasket(item: Item, userId: UUID): F[Unit] = {
+  def addToBasket(item: Item, userId: UUID): F[Unit] =
     basketRef.update(
-      userToBasket => userToBasket.get(userId) match {
-        case Some(basket) if basket.contains(item.symbol) => userToBasket ++ Map(userId -> (basket ++ Map(item.symbol -> basket(item.symbol))))
-        case Some(basket) => userToBasket ++ Map(userId -> (basket ++ Map(item.symbol -> item.quantity)))
-        case None => userToBasket ++ Map(userId -> Map.empty)
+      globalBasket => globalBasket.get(userId) match {
+        case Some(userBasket) if userBasket.contains(item.symbol) =>
+          val newQuantity = (userBasket(item.symbol) + item.quantity)
+          val updatedUserBasket = userBasket + (item.symbol -> newQuantity)
+          globalBasket + (userId -> updatedUserBasket)
+        case Some(userBasket) =>
+          val updatedUserBasket = userBasket + (item.symbol -> item.quantity)
+          globalBasket + (userId -> updatedUserBasket)
+        case None =>
+          val newUserBasket = Map(item.symbol -> item.quantity)
+          globalBasket + (userId -> newUserBasket)
       }
     )
-  }
 
   def viewBasket(userId: UUID)(implicit me: MonadError[F, Throwable]): F[Map[String, Int]] = basketRef.get.map(basket => basket.getOrElse(userId, Map.empty))
 }
